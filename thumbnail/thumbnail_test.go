f93b8d1ceb534ff0f1fe3df81f2df89dc2822cb4
Paik Gyeongjun <paik625@naver.com>
2019-11-12 01:19:14 +0900

    save file

---
+++ thumbnail/thumbnail_test.go
@@ -0,0 +1,10 @@
+import "log"
+
+// makeThumbnails makes thumbnails of the specified files.
+func makeThumbnails(filenames []string) {
+	for _, f := range filenames {
+		if _, err := thumbnail.ImageFile(f); err != nil {
+			log.Println(err)
+		}
+	}
+}
